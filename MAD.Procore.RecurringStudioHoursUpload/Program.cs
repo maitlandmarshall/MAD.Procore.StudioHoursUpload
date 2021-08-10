c33c4d7bc5c18e48e533eb345158016d6d95ee37
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-08-10 11:08:39 +1000

    Add project files.

---
+++ MAD.Procore.RecurringStudioHoursUpload/Program.cs
@@ -0,0 +1,21 @@
+﻿using MAD.Integration.Common;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace MAD.Procore.RecurringStudioHoursUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            IntegrationHost.CreateDefaultBuilder(args)
+                .UseAspNetCore()
+                .UseAppInsights()
+                .UseStartup<Startup>();
+    }
+}
---
+++ MAD.Procore.RecurringStudioHoursUpload/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace MAD.Procore.RecurringStudioHoursUpload
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection serviceDescriptors)
+        {
+
+        }
+
+        public async Task Configure(IGlobalConfiguration hangfireConfig)
+        {
+
+        }
+    }
+}
